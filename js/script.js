c06e5f59a17d3f05540118ec698c562420d3eb4e
Gias uddin vuiya <giasuddin6900@gmail.com>
2022-02-17 23:34:01 +0600

    updated virsion one

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+/*Selecting eliment */
+const box1Heading2 = document.querySelector('.box-1 h2');
+const box1Btn1 = document.querySelector('.box-1 button');
+
+box1Btn1.addEventListener('click',function()
+{
+   console.log('Hello world');
+})
+
