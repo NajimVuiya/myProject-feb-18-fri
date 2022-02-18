daf4b06f9d7b80d26c51d3fa9f6fba6e552cc35d
Gias uddin vuiya <giasuddin6900@gmail.com>
2022-02-18 12:14:04 +0600

    My updeted virsion FIVE

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@
 /*Selecting eliment */
 const box1Heading2 = document.querySelector('.box-1 h2');
 const box1Btn1 = document.querySelector('.box-1 button');
-
 const box2Heading2 = document.querySelector('.box-2 h2');
 const box2Btn1 = document.querySelector('.box-2 button');
-
 const box3 = document.querySelector('.box-3');;
-
+const box1Btn1 = document.querySelector('.box-3 button strong');
